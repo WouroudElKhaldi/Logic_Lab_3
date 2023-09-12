9067cb2b7a1e30fa91b418fbd9e39bb2a6e3afc0
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-09-12 17:07:45 +0300

    finished the Ex5 , and it is already a function

---
+++ Wouroud_Mohamad_5.js
@@ -0,0 +1,7 @@
+
+var number = parseFloat(prompt("Enter a number"))
+function square(number) {
+    return number * number ;
+} ;
+var squareNum = square(number) ;
+alert("The square of this number is " + squareNum) ;
